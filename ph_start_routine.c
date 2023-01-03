7af65e60f3e5962cd215fceadd092eb1c7681527
hioikawa <uminomae.com>
2023-01-03 23:15:44 +0900

    関数エラー時処理

--- ph_start_routine.c
+++ ph_start_routine.c
@@ -1,12 +1,13 @@
 #include "philo.h"
 
 static int food_on_table(t_philo *ph);
-static void toggle_mutex_forks(t_philo *ph, size_t c, size_t j);
+static void toggle_mutex_forks(t_list *list, size_t c, size_t j);
 
 void    *dining_philosophers(void *ptr)
 {
-	size_t	f;
+	size_t  f;
     t_philo *ph;
+    int ret;
 
     ph = (t_philo *)ptr;
     printf ("Philosopher %zu is done thinking and now ready to eat.\n", ph->id);
@@ -16,35 +17,55 @@ void    *dining_philosophers(void *ptr)
             break;//プログラムの終了
         if (ph->id == 1)
             sleep (ph->sleep_seconds);//->usleep
-        toggle_mutex_forks(ph, ph->id, 1);
+        toggle_mutex_forks(&ph->fork_list, ph->id, 1);
         put_timestamp(ph, ph->id, EATING);
-        usleep (DELAY * (FOOD - f + 1));
-        toggle_mutex_forks(ph, ph->id, 0);
+        ret = usleep (DELAY * (FOOD - f + 1));
+        if (ret != 0)
+            exit(1);
+        toggle_mutex_forks(&ph->fork_list, ph->id, 0);
     }
     printf ("Philosopher %zu is done eating.\n", ph->id);
     return (NULL);
 }
 
-static void toggle_mutex_forks(t_philo *ph, size_t c, size_t j) {
-    size_t	i;
-    t_node	*node;
+static void lock_mutex(t_node *node)
+{
+    int ret;
 
-	node = ph->fork_list.head;
+    ret = pthread_mutex_lock(&node->mutex);
+    if (ret != 0)
+        exit(1);
+}
+
+static void unlock_mutex(t_node *node)
+{
+    int ret;
+
+    ret = pthread_mutex_unlock(&node->mutex);
+    if (ret != 0)
+        exit(1);
+}
+
+static void toggle_mutex_forks(t_list *list, size_t c, size_t j) {
+    size_t	i;
+    t_node	*node_fork;
+
+	node_fork = list->head;
     i = 0;
     while(i < c)
     {
-        node = node->next;
+        node_fork = node_fork->next;
         i++;
     }
     if (j == 1)
     {
-        pthread_mutex_lock(&node->mutex);
-        pthread_mutex_lock(&node->next->mutex);
+        lock_mutex(node_fork);
+        lock_mutex(node_fork->next);
         printf ("fork's mutex lock:got LR-hand fork %zu\n", c);
     }else if (j == 0)
     {
-        pthread_mutex_unlock(&node->mutex);
-        pthread_mutex_unlock(&node->next->mutex);
+        unlock_mutex(node_fork);
+        unlock_mutex(node_fork->next);
         printf ("fork's mutex unlock %zu\n", c);
     }
 }
--- ph_thread.c
+++ ph_thread.c
@@ -5,15 +5,19 @@ void    init_pthread_mutex(t_philo *ph)
     size_t i;
     size_t num_philo;
 	t_node *node;
+    int ret;
 
 	node = ph->fork_list.head;
     num_philo = ph->argv[1];
-    pthread_mutex_init(&ph->food_lock, NULL);
+    ret = pthread_mutex_init(&ph->food_lock, NULL);
+    if (ret != 0)
+        exit(1);
     i = 0;
     while (i < num_philo)
     {
-		// printf("pthre_mu\n");
-        pthread_mutex_init(&node->mutex, NULL);
+        ret = pthread_mutex_init(&node->mutex, NULL);
+        if (ret != 0)
+            exit(1);
 		node = node->next;
         i++;
     }
@@ -23,13 +27,16 @@ void    create_pthread(t_philo *ph)
 {
     size_t i;
     size_t num_philo;
+    int ret;
 
     num_philo = ph->argv[1];
     i = 0; 
     while (i < num_philo)
     {
         ph->id = i;
-        pthread_create(&ph->philo[i], NULL, dining_philosophers, ph);
+        ret = pthread_create(&ph->philo[i], NULL, dining_philosophers, ph);
+        if (ret != 0)
+            exit(1);
         i++;
     }
 }
@@ -38,12 +45,15 @@ void    join_pthread(t_philo *ph)
 {
     size_t i;
     size_t num_philo;
+    int ret;
 
     num_philo = ph->argv[1];
     i = 0;
     while (i < num_philo)
     {
-        pthread_join(ph->philo[i], NULL);
+        ret = pthread_join(ph->philo[i], NULL);
+        if (ret != 0)
+            exit(1);
         i++;
     }
 }
