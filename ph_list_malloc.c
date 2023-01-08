4f1631756262fed6e44281ab111825c4281f1a6c
hioikawa <uminomae.com>
2023-01-08 23:53:27 +0900

    mallc　NULL処理

--- ph_list_malloc.c
+++ ph_list_malloc.c
@@ -6,7 +6,7 @@
 /*   By: uminomae <uminomae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 00:43:12 by uminomae          #+#    #+#             */
-/*   Updated: 2023/01/06 22:31:21 by uminomae         ###   ########.fr       */
+/*   Updated: 2023/01/08 23:51:33 by uminomae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static t_ptr_node	*init_ptr_node(void)
 
 	node = (t_ptr_node *)malloc(sizeof(t_ptr_node));
 	if (node == NULL)
-		exit(1);
+		return (NULL);
 	node->ptr = NULL;
 	node->next = NULL;
 	return (node);
@@ -41,6 +41,8 @@ static void	add_ptr_list(t_ptr_list *ptr_list, void *ptr)
 	t_ptr_node	*node;
 
 	node = init_ptr_node();
+	if (node == NULL)
+		return ;
 	node->ptr = ptr;
 	if (ptr_list->head == NULL)
 		make_first_ptr_node(ptr_list, node);
