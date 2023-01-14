7d571c9fd99223e1a45bdc9c3fc3710386940aad
hioikawa <uminomae.com>
2023-01-14 23:40:44 +0900

    monitor削減

--- philo/srcs/ph_begin_build.c
+++ philo/srcs/ph_begin_build.c
@@ -6,7 +6,7 @@
 /*   By: uminomae <uminomae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 15:21:53 by uminomae          #+#    #+#             */
-/*   Updated: 2023/01/13 18:39:44 by uminomae         ###   ########.fr       */
+/*   Updated: 2023/01/14 23:40:29 by uminomae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ static void	make_list(t_philo_main *ph)
 			get_err_num_ph(ph, ERR_ADD_PHILO_LIST);
 		if (add_fork_list(ph, &ph->fork_list, &ph->alloc_list, id) == 1)
 			get_err_num_ph(ph, ERR_ADD_FORK_LIST);
-		if (add_monitor_list(ph, &ph->monitor_list, &ph->alloc_list, id) == 1)
-			get_err_num_ph(ph, ERR_ADD_MONITOR_LIST);
+		// if (add_monitor_list(ph, &ph->monitor_list, &ph->alloc_list, id) == 1)
+		// 	get_err_num_ph(ph, ERR_ADD_MONITOR_LIST);
 		id++;
 	}
 }
