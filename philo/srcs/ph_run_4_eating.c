04b728f688910b1c9a8f7b910eaa31bc15e239f4
hioikawa <uminomae.com>
2023-01-16 23:36:45 +0900

    lock_fork_mutex 全員nextは2番目にteken

--- philo/srcs/ph_run_4_eating.c
+++ philo/srcs/ph_run_4_eating.c
@@ -6,15 +6,14 @@
 /*   By: uminomae <uminomae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 01:04:10 by uminomae          #+#    #+#             */
-/*   Updated: 2023/01/16 23:29:18 by uminomae         ###   ########.fr       */
+/*   Updated: 2023/01/16 23:35:47 by uminomae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 // static bool	case_tail_person(t_philo_node *node_th);
-static void	unlock_mutex_forks(t_philo_node *node_philo, \
-		t_fork_node *node_fork);
+static void	unlock_mutex_forks(t_fork_node *node_fork);
 static bool	lock_fork_mutex(t_philo_node *node_philo, \
 		t_fork_node *node_fork, size_t id);
 static bool	lock_next_fork_mutex(t_philo_node *node_philo, \
@@ -71,10 +70,10 @@ bool	run_eating(t_philo_node *node_philo, \
 		return (false);
 	if (!put_state(EATING, node_philo, time_eat, id))
 	{
-		unlock_mutex_forks(node_philo, node_fork);
+		unlock_mutex_forks(node_fork);
 		return (false);
 	}
-	unlock_mutex_forks(node_philo, node_fork);
+	unlock_mutex_forks(node_fork);
 	x_lock_mutex_philo(node_philo);
 	node_philo->cnt++;
 	x_unlock_mutex_philo(node_philo);
@@ -91,58 +90,26 @@ bool	run_eating(t_philo_node *node_philo, \
 	return (true);
 }
 
-// static bool	case_tail_person(t_philo_node *node_th)
-// {
-// 	if (node_th == node_th->ph->philo_list.tail)
-// 		return (true);
-// 	return (false);
-// }
 
-static void	unlock_mutex_forks(t_philo_node *node_philo, \
-		t_fork_node *node_fork)
+static void	unlock_mutex_forks(t_fork_node *node_fork)
 {
 	t_fork_node		*node_next_fork;
 
-	(void)node_philo;
 	node_next_fork = node_fork->next;
-	// if (case_tail_person(node_philo))
-	// {
-	// 	x_unlock_mutex_fork(node_fork);
-	// 	x_unlock_mutex_fork(node_next_fork);
-	// }
-	// else
-	// {
-		x_unlock_mutex_fork(node_next_fork);
-		x_unlock_mutex_fork(node_fork);
-	// }
+	x_unlock_mutex_fork(node_next_fork);
+	x_unlock_mutex_fork(node_fork);
 }
 
 static bool	lock_fork_mutex(t_philo_node *node_philo, \
 		t_fork_node *node_fork, size_t id)
 {
-	// t_fork_node	*node_next_fork;
 
-	// x_lock_mutex_fork(node_fork);
-	// node_next_fork = node_fork->next;
-	// x_unlock_mutex_fork(node_fork);
-	// if (case_tail_person(node_philo))
-	// {
-	// 	x_lock_mutex_fork(node_next_fork);
-	// 	if (!put_state(TAKEN_FORK, node_philo, 0, id))
-	// 	{
-	// 		x_unlock_mutex_fork(node_next_fork);
-	// 		return (false);
-	// 	}
-	// }
-	// else
-	// {
-		x_lock_mutex_fork(node_fork);
-		if (!put_state(TAKEN_FORK, node_philo, 0, id))
-		{
-			x_unlock_mutex_fork(node_fork);
-			return (false);
-		}
-	// }
+	x_lock_mutex_fork(node_fork);
+	if (!put_state(TAKEN_FORK, node_philo, 0, id))
+	{
+		x_unlock_mutex_fork(node_fork);
+		return (false);
+	}
 	return (true);
 }
 
@@ -152,23 +119,11 @@ static bool	lock_next_fork_mutex(t_philo_node *node_philo, \
 	t_fork_node	*node_next_fork;
 
 	node_next_fork = node_fork->next;
-	// if (case_tail_person(node_philo))
-	// {
-	// 	x_lock_mutex_fork(node_fork);
-	// 	if (!put_state(TAKEN_FORK, node_philo, 0, id))
-	// 	{
-	// 		x_unlock_mutex_fork(node_fork);
-	// 		return (false);
-	// 	}
-	// }
-	// else
-	// {
-		x_lock_mutex_fork(node_next_fork);
-		if (!put_state(TAKEN_FORK, node_philo, 0, id))
-		{
-			x_unlock_mutex_fork(node_next_fork);
-			return (false);
-		}
-	// }
+	x_lock_mutex_fork(node_next_fork);
+	if (!put_state(TAKEN_FORK, node_philo, 0, id))
+	{
+		x_unlock_mutex_fork(node_next_fork);
+		return (false);
+	}
 	return (true);
 }
