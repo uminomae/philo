b1956d0b39d9f9b9fa5f53fca38068b05c7b65ad
hioikawa <uminomae.com>
2023-01-18 23:40:33 +0900

    get_error_num_ptr

--- philo/srcs/ph_error_check.c
+++ philo/srcs/ph_error_check.c
@@ -6,28 +6,28 @@
 /*   By: uminomae <uminomae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 10:21:59 by uminomae          #+#    #+#             */
-/*   Updated: 2023/01/18 18:06:09 by uminomae         ###   ########.fr       */
+/*   Updated: 2023/01/18 23:40:01 by uminomae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	check_error_num_ptr(t_philo_main *ph);
-static void	check_error_num_list_fork(t_philo_main *ph);
-static void	check_error_num_list_philo(t_philo_main *ph);
+static void	get_error_num_ptr(t_philo_main *ph);
+static void	get_error_num_list_fork(t_philo_main *ph);
+static void	get_error_num_list_philo(t_philo_main *ph);
 
 bool	is_error(t_philo_main *ph)
 {
 	ph->error_num = ph->mutex_struct.error_num;
-	check_error_num_ptr(ph);
-	check_error_num_list_fork(ph);
-	check_error_num_list_philo(ph);
+	get_error_num_ptr(ph);
+	get_error_num_list_fork(ph);
+	get_error_num_list_philo(ph);
 	if (ph->error_num > NUM_ERR_LOW)
 		return (true);
 	return (false);
 }
 
-static void	check_error_num_ptr(t_philo_main *ph)
+static void	get_error_num_ptr(t_philo_main *ph)
 {
 	t_ptr_node	*node_ptr;
 	size_t		i;
@@ -47,7 +47,7 @@ static void	check_error_num_ptr(t_philo_main *ph)
 	return ;
 }
 
-static void	check_error_num_list_fork(t_philo_main *ph)
+static void	get_error_num_list_fork(t_philo_main *ph)
 {
 	t_fork_node	*node_fork;
 	size_t		i;
@@ -67,7 +67,7 @@ static void	check_error_num_list_fork(t_philo_main *ph)
 	return ;
 }
 
-static void	check_error_num_list_philo(t_philo_main *ph)
+static void	get_error_num_list_philo(t_philo_main *ph)
 {
 	t_philo_node	*node_philo;
 	size_t			i;
@@ -78,7 +78,9 @@ static void	check_error_num_list_philo(t_philo_main *ph)
 	{
 		if (node_philo->error_num > NUM_ERR_LOW)
 		{
+			x_lock_mutex_ph(&ph->mutex_ph, ph);
 			ph->error_num = node_philo->error_num;
+			x_unlock_mutex_ph(&ph->mutex_ph, ph);
 			return ;
 		}
 		node_philo = node_philo->next;
