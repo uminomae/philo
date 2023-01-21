7d6de324cd493959522e430fcf3b58fe2a7e318a
hioikawa <uminomae.com>
2023-01-21 18:56:34 +0900

    get_err_num

--- philo/srcs/ph_util_mutex_struct.c
+++ philo/srcs/ph_util_mutex_struct.c
@@ -6,7 +6,7 @@
 /*   By: uminomae <uminomae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 00:52:51 by uminomae          #+#    #+#             */
-/*   Updated: 2023/01/21 09:11:19 by uminomae         ###   ########.fr       */
+/*   Updated: 2023/01/21 18:56:21 by uminomae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	x_lock_mutex_ph(pthread_mutex_t *mutex_ph, t_ph *ph)
 {
 	if (pthread_mutex_lock(mutex_ph) != 0)
 	{
-		x_lock_mutex_ph(mutex_ph, ph);
+		// x_lock_mutex_ph(mutex_ph, ph);
 		get_err_num_ph(ph, ERR_PTHREAD_LOCK);
-		x_unlock_mutex_ph(mutex_ph, ph);
+		// x_unlock_mutex_ph(mutex_ph, ph);
 	}
 }
 
@@ -26,9 +26,9 @@ void	x_unlock_mutex_ph(pthread_mutex_t *mutex_ph, t_ph *ph)
 {
 	if (pthread_mutex_unlock(mutex_ph) != 0)
 	{
-		x_lock_mutex_ph(mutex_ph, ph);
+		// x_lock_mutex_ph(mutex_ph, ph);
 		get_err_num_ph(ph, ERR_PTHREAD_UNLOCK);
-		x_unlock_mutex_ph(mutex_ph, ph);
+		// x_unlock_mutex_ph(mutex_ph, ph);
 	}
 }
 
@@ -36,9 +36,9 @@ void	x_lock_mutex_struct(pthread_mutex_t *mutex, t_mutex *mtx_st)
 {
 	if (pthread_mutex_lock(mutex) != 0)
 	{
-		x_lock_mutex_struct(&mtx_st->mtx_ate_all, mtx_st);
+		// x_lock_mutex_struct(&mtx_st->mtx_ate_all, mtx_st);
 		get_err_num_mutex(mtx_st, ERR_PTHREAD_LOCK);
-		x_unlock_mutex_struct(&mtx_st->mtx_ate_all, mtx_st);
+		// x_unlock_mutex_struct(&mtx_st->mtx_ate_all, mtx_st);
 	}
 }
 
@@ -46,8 +46,8 @@ void	x_unlock_mutex_struct(pthread_mutex_t *mutex, t_mutex *mtx_st)
 {
 	if (pthread_mutex_unlock(mutex) != 0)
 	{
-		x_lock_mutex_struct(&mtx_st->mtx_ate_all, mtx_st);
+		// x_lock_mutex_struct(&mtx_st->mtx_ate_all, mtx_st);
 		get_err_num_mutex(mtx_st, ERR_PTHREAD_UNLOCK);
-		x_unlock_mutex_struct(&mtx_st->mtx_ate_all, mtx_st);
+		// x_unlock_mutex_struct(&mtx_st->mtx_ate_all, mtx_st);
 	}
 }
