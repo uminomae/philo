391111ed530b1b60d79e324e68fed6e80e02cf3f
hioikawa <uminomae.com>
2023-01-23 23:18:40 +0900

    get_philo mutex

--- philo/srcs/ph_run_3_hungry.c
+++ philo/srcs/ph_run_3_hungry.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ph_run_monitor_hungry.c                            :+:      :+:    :+:   */
+/*   ph_run_3_hungry.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: uminomae <uminomae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 00:52:51 by uminomae          #+#    #+#             */
-/*   Updated: 2023/01/23 13:26:10 by uminomae         ###   ########.fr       */
+/*   Updated: 2023/01/23 23:15:37 by uminomae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,9 @@ static bool	judge_hungry(t_ph *ph, size_t num_people)
 	i = 0;
 	while (i < num_people)
 	{
+		x_lock_mutex_philo(ph->philo_list.head);
 		node_philo = get_philo(&ph->philo_list, i);
+		x_unlock_mutex_philo(ph->philo_list.head);
 		x_lock_mutex_philo(node_philo);
 		ret = check_time_ate(ph, node_philo);
 		if (ret == HUNGRY)
